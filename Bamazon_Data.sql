1790de1b1a06fe44cc9f9b325f84fe7fe4d62888
Emily Keen <emily.keen711@gmail.com>
2017-04-18 19:01:03 -0400

    first part done

---
+++ Bamazon_Data.sql
@@ -0,0 +1,33 @@
+use Bamazon_db;
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quanity)
+VALUES ("1", "printer", "Home", 50.99, 15);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quanity)
+VALUES ("2", "record player", "Electronics", 77.87, 18);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quanity)
+VALUES ("3", "sunglasses", "Clothing", 15, 100);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quanity)
+VALUES ("4", "tanktop", "Clothing", 7.99, 60);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quanity)
+VALUES ("5", "Web Design Text Book", "Books", 32.25, 10);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quanity)
+VALUES ("6", "high chair", "Baby", 69.99, 20);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quanity)
+VALUES ("7", "iPhone 7 case", "electronics", 12.99, 12);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quanity)
+VALUES ("8", "Dog Bed", "pet", 25.99, 20);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quanity)
+VALUES ("9", "blanket", "Home", 45, 18);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quanity)
+VALUES ("10", "blanket", "Clothing", 26.00, 7);
+
+select * From products;
---
+++ bamazonCustomer.js
@@ -0,0 +1,64 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    // Your username
+    user: "emilykeen711",
+    // Your password
+    password: "password123",
+    database: "Bamazon_db"
+});
+// connect to the mysql server and sql database
+connection.connect(function(err) {
+    if (err) throw err;
+});
+
+
+var start = function() {
+    connection.query("SELECT * FROM products", function(err, results) {
+        if (err) throw err;
+        console.log(results);
+
+        //put query "select * From products;"
+        inquirer.prompt([{
+            name: "ID",
+            type: "input",
+            message: "What is the ID of the item you would like to purchase?"
+        }, {
+            name: "quantity",
+            type: "input",
+            message: "What quantity of this product would you like?"
+        }]).then(function(answer) {
+
+            var chosenItem;
+            for (var i = 0; i < results.length; i++) {
+                if (results[i].item_id == answer.ID) {
+                    chosenItem = results[i];
+                    console.log(chosenItem);
+                    var quantity = chosenItem.stock_quanity - answer.quantity;
+                    console.log(quantity);
+                    if (chosenItem.stock_quanity >= answer.quantity) {
+                        connection.query("UPDATE products SET ? WHERE ?", [{
+                            quantity: quantity
+                        }, {
+                            ID: answer.ID
+                        }], function(err, res) {
+                            console.log("Your Total is:"+ chosenItem.price * answer.quantity);
+
+
+                        });
+                    }else{
+                        console.log("Insufficient quantity!");
+                    }
+                }
+            }
+
+
+            //start();
+        })
+    });
+}
+
+start();
